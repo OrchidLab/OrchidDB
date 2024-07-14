f1b39b16237bca3e8be1f6ebe1f6823d49dc63b8
aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>
2024-07-14 16:23:05 -0700

    WAL Support
    Signed-off-by: aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>

--- src/main.zig
+++ src/main.zig
@@ -4,12 +4,20 @@ const OrchardDB = @import("orchard.zig").OrchardDB;
 pub fn main() !void {
     var allocator = std.heap.GeneralPurposeAllocator(.{}){};
     const gpa = allocator.allocator();
-    var orchard = try OrchardDB.init(gpa, "./.orchard.1.db", "./.orchard.1.log", 128);
+    var orchard = try OrchardDB.init(gpa, "./.tmp/.orchard.prd1.db", "./.tmp/.orchard.prd1.log", 512);
     defer orchard.deinit();
-
-    for (0..1000) |char| {
-        const ch: u8 = @intCast(char);
-        try orchard.put(&[_]u8{ch}, &[_]u8{ch});
+    std.debug.print("[INFO] Begin insert of 4095 items, evey 100th item a key will be clobbered. \n", .{});
+    var keyb: [16]u8 = undefined;
+    var valb: [16]u8 = undefined;
+    for (1..4096) |i| {
+        if (i % 100 == 0) {
+            const key = try std.fmt.bufPrint(&keyb, "KEY{d}", .{i - 1});
+            try orchard.delete(key);
+        } else {
+            const key = try std.fmt.bufPrint(&keyb, "KEY{d}", .{i});
+            const value = try std.fmt.bufPrint(&valb, "VAL{d}", .{i});
+            try orchard.put(key, value);
+        }
     }
-    std.debug.print("[INFO] Done\n", .{});
+    std.debug.print("[INFO] Insert ended. \n", .{});
 }
--- src/orchard.zig
+++ src/orchard.zig
@@ -17,7 +17,7 @@ pub const OrchardDB = struct {
 
     pub fn init(allocator: std.mem.Allocator, disk_path: []const u8, log_path: []const u8, persistant_to: usize) !@This() {
         const disk = try std.fs.cwd().createFile(disk_path, .{ .read = true, .truncate = false });
-        const log = try std.fs.cwd().createFile(log_path, .{ .read = true });
+        const log = try std.fs.cwd().createFile(log_path, .{ .read = true, .truncate = false });
         const hashmap = try allocator.create(map.HashMap);
         errdefer allocator.destroy(hashmap);
         hashmap.* = try map.HashMap.init(allocator, 1024, (3 / 4));
@@ -34,7 +34,7 @@ pub const OrchardDB = struct {
         try self.write_to_log("PUT", key, value);
         try self.hashmap.set(key, value);
         self.operations += 1;
-        if (self.operations > self.persistant_to) try self.write_to_disk();
+        if (self.operations > self.persistant_to) try self.sync();
     }
 
     pub fn get(self: *@This(), key: []const u8) ?[]const u8 {
@@ -42,8 +42,43 @@ pub const OrchardDB = struct {
     }
 
     pub fn delete(self: *@This(), key: []const u8) !void {
-        try self.write_to_log("DELETE", key);
+        try self.write_to_log("DELETE", key, "");
         try self.hashmap.delete(key);
+    }
+
+    fn sync(self: *@This()) !void {
+        try self.write_to_disk();
+        try self.log.setEndPos(0);
+        try self.log.sync();
+        self.operations = 0;
+    }
+
+    fn recover(self: *@This()) !void {
+        var disk_buffer: [4096]u8 = undefined;
+        while (try self.disk.reader().readUntilDelimiterOrEof(&disk_buffer, '\n')) |line| {
+            var token = std.mem.tokenizeAny(u8, line, " ");
+            const key = token.next() orelse continue;
+            const value = token.next() orelse continue;
+            try self.hashmap.set(key, value);
+        }
+
+        var log_buffer: [4096]u8 = undefined;
+        try self.log.seekTo(0);
+        if (try self.log.getEndPos() == 0) return;
+
+        while (try self.log.reader().readUntilDelimiterOrEof(&log_buffer, '\n')) |line| {
+            std.debug.print("line {s}", .{line});
+            var token = std.mem.tokenizeAny(u8, line, " ");
+            const operation = token.next() orelse continue;
+            const key = token.next() orelse continue;
+            const value = token.next() orelse continue;
+
+            if (std.mem.eql(u8, operation, "PUT")) {
+                try self.hashmap.set(key, value);
+            } else if (std.mem.eql(u8, operation, "DELETE")) {
+                self.hashmap.delete(key) catch {};
+            }
+        }
     }
 
     fn write_to_disk(self: *@This()) !void {
@@ -56,7 +91,6 @@ pub const OrchardDB = struct {
             }
         }
         try self.disk.sync();
-        self.operations = 0;
     }
 
     fn write_to_log(self: *@This(), operation: []const u8, key: []const u8, value: []const u8) !void {
@@ -70,10 +104,17 @@ pub const OrchardDB = struct {
 
 test "Key Value Store" {
     const alloc = std.testing.allocator;
-    var orchard = try OrchardDB.init(alloc, "./.orchard.1.db", "./.orchard.1.log", 64);
+    var orchard = try OrchardDB.init(alloc, "../.tmp/.orchard.test.db", "../.tmp/.orchard.test.log", 500);
     defer orchard.deinit();
-    _ = try orchard.put("key", "1");
-    try std.testing.expectEqualStrings("1", orchard.get("key").?);
-    _ = try orchard.delete("key");
-    try std.testing.expectEqual(null, orchard.get("key"));
+    orchard.hashmap.debug("nothing", true);
+    try orchard.recover();
+    orchard.hashmap.debug("should have 6 keys", true);
+    // _ = try orchard.put("key", "1");
+    // orchard.hashmap.debug("key1", true);
+    // _ = try orchard.put("key1", "2");
+    // _ = try orchard.put("key2", "3");
+    // _ = try orchard.put("key3", "4");
+    // try std.testing.expectEqualStrings("1", orchard.get("key").?);
+    // _ = try orchard.delete("key");
+    // try std.testing.expectEqual(null, orchard.get("key"));
 }
